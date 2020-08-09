37d395b6313574532426ed5a6efe2e3487a11fd6
Rishabh Rawat <rishabhrawat192@gmail.com>
2020-08-09 13:25:33 +0530

    added product remove feature in website

---
+++ public/js/sellerProducts.js
@@ -0,0 +1,29 @@
+/*eslint-disable*/
+const buttonRemove = document.querySelectorAll(".button-remove");
+if (buttonRemove) {
+    buttonRemove.forEach(button => {
+        button.addEventListener("click", function() {
+            const id = this.dataset.id;
+            if (confirm("Do you really want to delete this product?")) {
+                deleteRequest(id);
+            }
+        });
+    });
+}
+
+async function deleteRequest(productId) {
+    try {
+        const res = await axios({
+            method: "DELETE",
+            url: `/api/v1/products/${productId}`
+        });
+        if (res.status === 204) {
+            alert(`Successfully removed!`);
+            return location.assign("/products");
+        }
+        console.log(res);
+        alert("OOps something went wrong");
+    } catch (err) {
+        alert(err.response.data.message);
+    }
+}
